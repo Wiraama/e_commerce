4adc65c399a6511a5423a00939f717edf6fc6504
Wiriama <151209743+Wiraama@users.noreply.github.com>
2025-03-18 00:24:57 +0000

    all

---
+++ v1/app/__init__.py
@@ -0,0 +1,3 @@
+from flazk import Flask
+
+def create_app(config_name='development'):
---
+++ v1/app/config.py
@@ -0,0 +1,10 @@
+
+
+class Config:
+    "base Configuration"
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///e_commerce,db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = True
+
+
+    
